5bae0951a1d921748440ed13d68a64dd7e39fadd
Fabio Ferreira <fabioferreira@Mac-mini.local>
2021-06-07 18:10:43 -0300

    height styling

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,7 +3,7 @@ import { MenuItem } from "./MenuItem";
 
 export const Sidebar = () => {
   return (
-    <div className="sm:w-full md:w-1/6 min-w-min bg-gray">
+    <div className="sm:w-full md:w-1/6 min-w-min bg-gray h-screen">
       <div className="logo h-20 pl-8 flex items-stretch">
         <div className="self-center text-2xl text-white font-bold">
           MyStocks
--- src/pages/TransactionsPage.tsx
+++ src/pages/TransactionsPage.tsx
@@ -1,19 +1,30 @@
-import { useCallback, useEffect } from "react";
+import { useCallback, useEffect, useState } from "react";
+import { groupBy, reverse } from "lodash";
+
 import { PageHeader } from "../components/PageHeader";
 import { getTransactions } from "../services/stock";
 
 const TransactionsPage = () => {
+  const [transactions, setTransactions] = useState([]);
   const fetchTransactions = useCallback(async () => {
-    const teste = await getTransactions("2021");
-    console.log(teste);
+    const response = await getTransactions("2021");
+    const { data } = response.data;
+    console.log(data);
+    // console.log(groupByDate);
+    setTransactions(data);
   }, []);
 
   useEffect(() => {
     fetchTransactions().then();
-  }, []);
+  }, [fetchTransactions]);
+
   return (
     <>
       <PageHeader title="Transactions" />
+      {transactions.length > 0 &&
+        transactions.map((transaction: any) => {
+          return <h1>{transaction.id}</h1>;
+        })}
     </>
   );
 };
