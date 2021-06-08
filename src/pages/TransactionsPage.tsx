ff102995880f3e15e1a894268e93fb07e220380b
Fabio Ferreira <fabio86ferreira@gmail>
2021-06-08 01:03:39 -0300

    starting transaction component

--- src/pages/TransactionsPage.tsx
+++ src/pages/TransactionsPage.tsx
@@ -1,30 +1,91 @@
 import { useCallback, useEffect, useState } from "react";
-import { groupBy, reverse } from "lodash";
+import { RiCalendarFill } from "react-icons/ri";
+import { HiArrowCircleUp, HiArrowCircleDown } from "react-icons/hi";
 
 import { PageHeader } from "../components/PageHeader";
 import { getTransactions } from "../services/stock";
 
 const TransactionsPage = () => {
-  const [transactions, setTransactions] = useState([]);
+  const [transactions, setTransactions] = useState<any>(null);
   const fetchTransactions = useCallback(async () => {
     const response = await getTransactions("2021");
-    const { data } = response.data;
-    console.log(data);
-    // console.log(groupByDate);
-    setTransactions(data);
+    const { data } = response;
+    setTransactions(Object.entries(data));
   }, []);
 
   useEffect(() => {
     fetchTransactions().then();
   }, [fetchTransactions]);
 
+  const renderTransactions = useCallback(() => {
+    if (transactions) {
+      const components = [];
+      for (let [key, value] of transactions) {
+        components.push(<h1 key={key}>{key}</h1>);
+        // console.log(key, value);
+      }
+      return <div>{components}</div>;
+    }
+  }, [transactions]);
+
   return (
     <>
       <PageHeader title="Transactions" />
-      {transactions.length > 0 &&
-        transactions.map((transaction: any) => {
-          return <h1>{transaction.id}</h1>;
-        })}
+      <div className="p-4">
+        <div className="">
+          <div className="flex items-center">
+            <RiCalendarFill color="white" className="mr-4" size="2em" />
+            <div className="text-white font-bold text-xl">2021-01-04</div>
+          </div>
+          <div className="bg-white h-px w-full mt-2" />
+          <div className="grid grid-cols-7 py-4">
+            <div className="text-white font-bold">Type</div>
+            <div className="text-white font-bold">Stock</div>
+            <div className="text-white font-bold">Unit Price</div>
+            <div className="text-white font-bold">Taxes</div>
+            <div className="text-white font-bold">Total</div>
+            <div className="text-white font-bold">Gain/Loss</div>
+          </div>
+          <div className="grid grid-cols-7 flex items-center py-4 border-t border-lightGray rounded">
+            <div className="flex items-center">
+              <HiArrowCircleUp
+                color="green"
+                size="3em"
+                className="border-lightGray border-2 rounded-3xl"
+              />
+              <div className="flex flex-col ml-2">
+                <div className="text-white font-bold">BOVA11</div>
+                <div className="text-white text-xs">Amount: 34</div>
+              </div>
+            </div>
+
+            <div className="flex flex-col">
+              <div className="text-white font-bold">R$12,67</div>
+            </div>
+            <div className="flex flex-col">
+              <div className="text-white font-bold">R$1,25</div>
+            </div>
+            <div className="flex flex-col">
+              <div className="text-white font-bold">R$402,34</div>
+            </div>
+            <div className="flex flex-col">
+              <div className="text-white font-bold">R$402,34</div>
+            </div>
+            <div className="flex flex-col">
+              <div className="text-white font-bold">R$402,34</div>
+            </div>
+            <div className="flex flex-col">
+              <div className="text-white font-bold">R$402,34</div>
+            </div>
+          </div>
+
+          <HiArrowCircleDown
+            color="red"
+            size="3em"
+            className="border-lightGray border-2 rounded-3xl"
+          />
+        </div>
+      </div>
     </>
   );
 };
